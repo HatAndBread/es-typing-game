d0f8d882c1be53ae793f5d24feee0324c36ece5b
Joshua Hume <valencelectron@hotmail.com>
2021-07-12 14:33:42 +0900

    removed useless migrations

--- db/migrate/20210711002300_remove_quiz_from_high_score.rb
+++
@@ -1,5 +0,0 @@
-class RemoveQuizFromHighScore < ActiveRecord::Migration[6.1]
-  def change
-    remove_reference :high_scores, :quiz, null: false, foreign_key: true
-  end
-end
--- db/migrate/20210711004232_remove_player_from_high_score.rb
+++
@@ -1,5 +0,0 @@
-class RemovePlayerFromHighScore < ActiveRecord::Migration[6.1]
-  def change
-    remove_reference :high_scores, :player, null: false, foreign_key: true
-  end
-end
